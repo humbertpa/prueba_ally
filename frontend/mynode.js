b8464317661ae6f2b5bfc03ae7a6056e87a1d5bc
humbertpa <humbertpal@gmail.com>
2024-10-26 18:03:04 -0600

    Ya se puede seleccionar el pais y la zonna horaria en dashboard. Se agregaron tambien los archivos de banderas

---
+++ frontend/mynode.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({ path: '.env' });;
+
+const envFile = `export const environment = {
+    weatherApiKey: "${process.env.WEATHER_API_KEY}"
+};`;
+
+const targetPath = path.join(__dirname, 'src/environments/environment.development.ts');
+const targetProdPath = path.join(__dirname, 'src/environments/environment.ts');
+
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+        console.log(envFile)
+    }
+});
+
+fs.writeFile(targetProdPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+        console.log(envFile)
+    }
+});
--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  paises: string[] = ['Mexico', 'Alemania', 'Francia']
+
+  mx: string[] = ["America/Mexico_City", "America/Cancun", "America/Merida", "America/Matamoros", "America/Monterrey", "America/Tijuana"]
+  fr: string[] = ["Europe/Paris"]
+  al: string[] = ["Europe/Berlin"]
+
+  zonas: string[] = []
+  clima: any
+  capital: any
+  hora: any
+
+
+  async ngOnInit(): Promise<void> {
+    this.obtenerClima('Mexico')
+  }
+
+  async obtenerClima(pais: string): Promise<void> {
+
+    const pais_eng = pais == 'Mexico' ? 'mexico' : pais == 'Alemania' ? 'germany' : 'france'
+
+    try {
+      const params = {
+        key: environment.weatherApiKey,
+        q: pais_eng
+      }
+
+      const response = await axios.get('http://api.weatherapi.com/v1//current.json', { params });
+
+      this.zonas = pais_eng == 'mexico' ? this.mx : pais_eng == 'germany' ? this.al : this.fr
+      this.obtenerHora(this.zonas[0])
+      this.clima = response.data.current
+      this.capital = response.data.location
+      console.log(JSON.stringify(response.data));
+      return response.data
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  async obtenerHora(zona: string): Promise<void> {
+    try {
+      const response = await axios.get(`http://worldtimeapi.org/api/timezone/${zona}`)
+      console.log(JSON.stringify(response.data));
+      const datetime = new Date(response.data.datetime);
+      console.log(datetime)
+      const formattedDate = formatDate(datetime, 'dd/MM/yyyy hh:mm:ss a', 'en-US', response.data.utc_offset);
+      this.hora = { fecha: formattedDate, zona }
+      console.log(formattedDate)
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 }
